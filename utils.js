fb1ab236f5b3cdadeb959e12bcffaa2f734ccb80
maimous@gmail.com <stefaleon@gmail.com>
2016-12-01 09:20:21 +0200

    testing async

--- utils.js
+++ utils.js
@@ -1,6 +1,24 @@
+module.exports.add = (a, b) => a + b;
+
+module.exports.asyncAdd = (a, b, callback) => {
+	setTimeout(() => {
+		callback(a + b);
+	}, 500);
+};
+
+
 module.exports.setName = (user, fullName) => {
 	var names = fullName.split(' ');
 	user.firstName = names[0];
 	user.lastName = names[1];
 	return user;
+}
+
+module.exports.asyncSetName = (user, fullName, callback) => {
+	var names = fullName.split(' ');
+	user.firstName = names[0];
+	user.lastName = names[1];
+	setTimeout(() => {
+		callback(user);
+	}, 500);	
 }
--- utils.test.js
+++ utils.test.js
@@ -2,6 +2,19 @@ const expect = require('expect');
 const utils = require('./utils');
 
 
+it('it should add two numbers', () => {
+	var sum = utils.add(13, 29);
+	expect(sum).toBe(42).toBeA('number');			
+});
+
+it('it should async add two numbers', (done) => {
+	utils.asyncAdd(13, 29, (sum) => {
+		expect(sum).toBe(42).toBeA('number');
+		done();
+	});
+});
+
+
 it('should set first and last name', () => {
 	var user = { location: 'Bikini Bottom' };
 	var res = utils.setName(user, 'Spongebob Squarepants');
@@ -13,3 +26,19 @@ it('should set first and last name', () => {
 		lastName: 'Squarepants'
 	});
 });
+
+
+it('should async set first and last name', (done) => {
+	var user = { location: 'Bikini Bottom' };
+	utils.asyncSetName(user, 'Spongebob Squarepants', (res) =>{
+		// res equals user
+		expect(res).toEqual(user);	
+		// firstName should be 'Spongebob' and lastName should be 'Squarepants'
+		expect(res).toInclude({
+			firstName: 'Spongebob',
+			lastName: 'Squarepants'
+		});
+		done();
+	});	
+});
+
